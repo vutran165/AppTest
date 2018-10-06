1032814046e8a9ff25a7f86cb09f101fbb7e744e
Trần Tuấn Vũ <vutran165@gmail.com>
2018-10-06 10:45:27 +0700

    222

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + ''));
+
+// Start the app by listening on the default Heroku port
+var port = process.env.PORT || 8888;
+app.listen(port, (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('server started port: ${port}');
+    }
+});
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
+console.log('Running');
+
